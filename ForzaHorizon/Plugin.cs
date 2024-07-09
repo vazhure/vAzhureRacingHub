45d90cc75e988adcf334321a60515450a496cee9
Andrey Zhuravlev <124382776+vazhure@users.noreply.github.com>
2024-07-09 17:12:18 +0300

    Add files via upload

--- ForzaHorizon/Plugin.cs
+++ ForzaHorizon/Plugin.cs
@@ -79,7 +79,7 @@ namespace ForzaHorizon
                     default:
                     case FHVersion.FH4: return new string[] { "ForzaHorizon4" }; break;
                     case FHVersion.FH5: return new string[] { "ForzaHorizon5" }; break;
-                    case FHVersion.ForzaMotorsport: return new string[] { "ForzaMotorSport" }; break;
+                    case FHVersion.ForzaMotorsport: return new string[] { "forza_gaming.desktop.x64_release_final" }; break;
                 }
             }
         }
@@ -167,6 +167,103 @@ namespace ForzaHorizon
 
             mCarData = mCarData ?? new TelemetryDataSet(this);
 
+            if (m_version == FHVersion.ForzaMotorsport)
+            {
+                try
+                {
+                    FH8 dash = Marshalizable<FH8>.FromBytes(bytes);
+                    AMCarData cardata = mCarData.CarData;
+                    AMMotionData motionData = mCarData.CarData.MotionData;
+                    AMSessionInfo session = mCarData.SessionInfo;
+
+                    cardata.RPM = dash.CurrentEngineRpm;
+                    cardata.MaxRPM = dash.EngineMaxRpm;
+                    cardata.Brake = dash.Brake / 255.0f;
+                    cardata.Throttle = dash.Accel / 255.0f;
+                    cardata.Clutch = dash.Clutch / 255.0f;
+                    cardata.Gear = dash.Gear == 0 ? (short)0 : dash.Gear < 10 ? (short)(dash.Gear + 1) : (short)1;
+                    cardata.Speed = dash.Speed * 3.6f;
+                    cardata.Steering = dash.Steer / 127.0f;
+                    cardata.Position = dash.RacePosition;
+                    cardata.Lap = dash.LapNumber + 1;
+                    cardata.FuelLevel = dash.Fuel;
+
+                    // No motion in menu
+                    motionData.Yaw = dash.IsRaceOn > 0 ? (float)(dash.Yaw / Math.PI) : 0;
+                    motionData.Pitch = dash.IsRaceOn > 0 ? (float)(-dash.Pitch / Math.PI) : 0;
+                    motionData.Roll = dash.IsRaceOn > 0 ? (float)(-dash.Roll / Math.PI) : 0;
+                    motionData.Surge = dash.IsRaceOn > 0 ? dash.AccelerationZ / 30f : 0;
+                    motionData.Sway = dash.IsRaceOn > 0 ? -dash.AccelerationX / 30f : 0;
+                    motionData.Heave = dash.IsRaceOn > 0 ? dash.AccelerationY / 30f : 0;
+
+                    session.Flag = dash.IsRaceOn > 0 ? "Green" : "";
+                    session.SessionState = dash.IsRaceOn > 0 ? "Race" : "";
+                    session.BestLapTime = (int)(dash.BestLap * 1000f);
+                    session.CurrentLapTime = (int)(dash.CurrentLap * 1000f);
+                    session.CurrentLapNumber = dash.LapNumber + 1;
+                    session.CurrentPosition = dash.RacePosition;
+
+                    if (CarList.CarsFH8.TryGetValue(dash.CarOrdinal, out string carname))
+                        cardata.CarName = carname;
+                    else
+                        cardata.CarName = "Unknown model";
+
+                    if (Tracks.TrackList.TryGetValue(dash.TrackOrdinal, out string trackname))
+                        session.TrackName = trackname;
+                    else
+                        session.TrackName = "Unknown track";
+
+                    if (Tracks.TrackLength.TryGetValue(dash.TrackOrdinal, out float tracklen))
+                        session.TrackLength = tracklen * 1000f;
+                    else
+                        session.TrackLength = 1;
+
+                    cardata.Distance = dash.DistanceTraveled;
+
+                    cardata.Tires = new AMTireData[]
+                    {
+                        new AMTireData()
+                        {
+                            Pressure = 2.0,
+                            Temperature = new double[]{ dash.TireTempFrontLeft, dash.TireTempFrontLeft,dash.TireTempFrontLeft,dash.TireTempFrontLeft },
+                            BrakeTemperature = 100,
+                            Detached = false,
+                            Wear = dash.TireWearFrontLeft,
+                        },
+                        new AMTireData()
+                        {
+                            Pressure = 2.0,
+                            Temperature = new double[]{ dash.TireTempFrontRight, dash.TireTempFrontRight, dash.TireTempFrontRight, dash.TireTempFrontRight },
+                            BrakeTemperature = 100,
+                            Detached = false,
+                            Wear = dash.TireWearFrontRight,
+                        },
+                        new AMTireData()
+                        {
+                            Pressure = 2.0,
+                            Temperature = new double[]{ dash.TireTempRearLeft, dash.TireTempRearLeft, dash.TireTempRearLeft, dash.TireTempRearLeft },
+                            BrakeTemperature = 100,
+                            Detached = false,
+                            Wear = dash.TireWearRearLeft,
+                        },
+                        new AMTireData()
+                        {
+                            Pressure = 2.0,
+                            Temperature = new double[]{ dash.TireTempRearRight, dash.TireTempRearRight, dash.TireTempRearRight, dash.TireTempRearRight },
+                            BrakeTemperature = 100,
+                            Detached = false,
+                            Wear = dash.TireWearRearRight,
+                        },
+                    };
+
+                    bHasData = true;
+                }
+                catch { }
+
+                if (bHasData)
+                    OnTelemetry?.Invoke(this, new TelemetryUpdatedEventArgs(mCarData));
+            }
+            else
             if (bytes.Length >= FHDash.nSize)
             {
                 try
@@ -181,7 +278,7 @@ namespace ForzaHorizon
                     cardata.Brake = dash.Brake / 255.0f;
                     cardata.Throttle = dash.Accel / 255.0f;
                     cardata.Clutch = dash.Clutch / 255.0f;
-                    cardata.Gear = dash.Gear == 0 ? (short)0 : (short)(dash.Gear + 1);
+                    cardata.Gear = dash.Gear == 0 ? (short)0 : dash.Gear < 10 ?  (short)(dash.Gear + 1) : (short)1;
                     cardata.Speed = dash.Speed * 3.6f;
                     cardata.Steering = dash.Steer / 127.0f;
                     cardata.Position = dash.RacePosition;
--- ForzaHorizon/Structs.cs
+++ ForzaHorizon/Structs.cs
@@ -308,7 +308,6 @@ namespace ForzaMotorsport
         public int Unknown1;
         public int Unknown2;
 
-
         public float PositionX;
         public float PositionY;
         public float PositionZ;
@@ -376,5 +375,140 @@ namespace ForzaMotorsport
         /// Horizon specific car dash data
         /// </summary>
         public byte[] HorizonCarDash;
+    }
+
+    public struct FH8
+    {
+        // = 1 when race is on. = 0 when in menus/race stopped …
+        public int IsRaceOn;
+        
+        // Can overflow to 0 eventually
+        public uint TimestampMS;
+        public float EngineMaxRpm;
+        public float EngineIdleRpm;
+        public float CurrentEngineRpm;
+        
+        // In the car's local space; X = right, Y = up, Z = forward
+        public float AccelerationX;
+        public float AccelerationY;
+        public float AccelerationZ;
+        
+        // In the car's local space; X = right, Y = up, Z = forward
+        public float VelocityX;
+        public float VelocityY;
+        public float VelocityZ;
+        
+        // In the car's local space; X = pitch, Y = yaw, Z = roll
+        public float AngularVelocityX;
+        public float AngularVelocityY;
+        public float AngularVelocityZ;
+        
+        public float Yaw;
+        public float Pitch;
+        public float Roll;
+        
+        // Suspension travel normalized: 0.0f = max stretch; 1.0 = max compression
+        public float NormalizedSuspensionTravelFrontLeft;
+        public float NormalizedSuspensionTravelFrontRight;
+        public float NormalizedSuspensionTravelRearLeft;
+        public float NormalizedSuspensionTravelRearRight;
+        
+        // Tire normalized slip ratio, = 0 means 100% grip and |ratio| > 1.0 means loss of grip.
+        public float TireSlipRatioFrontLeft;
+        public float TireSlipRatioFrontRight;
+        public float TireSlipRatioRearLeft;
+        public float TireSlipRatioRearRight;
+        
+        // Wheels rotation speed radians/sec. 
+        public float WheelRotationSpeedFrontLeft;
+        public float WheelRotationSpeedFrontRight;
+        public float WheelRotationSpeedRearLeft;
+        public float WheelRotationSpeedRearRight;
+        
+        // = 1 when wheel is on rumble strip, = 0 when off.
+        public int WheelOnRumbleStripFrontLeft;
+        public int WheelOnRumbleStripFrontRight;
+        public int WheelOnRumbleStripRearLeft;
+        public int heelOnRumbleStripRearRight;
+        
+        // = from 0 to 1, where 1 is the deepest puddle
+        public float WheelInPuddleDepthFrontLeft;
+        public float WheelInPuddleDepthFrontRight;
+        public float WheelInPuddleDepthRearLeft;
+        public float WheelInPuddleDepthRearRight;
+        
+        // Non-dimensional surface rumble values passed to controller force feedback
+        public float SurfaceRumbleFrontLeft;
+        public float SurfaceRumbleFrontRight;
+        public float SurfaceRumbleRearLeft;
+        public float SurfaceRumbleRearRight;
+        
+        // Tire normalized slip angle, = 0 means 100% grip and |angle| > 1.0 means loss of grip.
+        public float TireSlipAngleFrontLeft;
+        public float TireSlipAngleFrontRight;
+        public float TireSlipAngleRearLeft;
+        public float TireSlipAngleRearRight;
+        
+        // Tire normalized combined slip, = 0 means 100% grip and |slip| > 1.0 means loss of grip.
+        public float TireCombinedSlipFrontLeft;
+        public float TireCombinedSlipFrontRight;
+        public float TireCombinedSlipRearLeft;
+        public float TireCombinedSlipRearRight;
+        
+        // Actual suspension travel in meters
+        public float SuspensionTravelMetersFrontLeft;
+        public float SuspensionTravelMetersFrontRight;
+        public float SuspensionTravelMetersRearLeft;
+        public float SuspensionTravelMetersRearRight;
+        
+        // Unique ID of the car make/model
+        public int CarOrdinal;
+        
+        // Between 0 (D -- worst cars) and 7 (X class -- best cars) inclusive         
+        public int CarClass;
+        
+        // Between 100 (worst car) and 999 (best car) inclusive
+        public int CarPerformanceIndex;
+        
+        // 0 = FWD, 1 = RWD, 2 = AWD
+        public int DrivetrainType;
+        
+        // Number of cylinders in the engine
+        public int NumCylinders;
+        public float PositionX;
+        public float PositionY;
+        public float PositionZ;
+        public float Speed;
+        public float Power;
+        public float Torque;
+        public float TireTempFrontLeft;
+        public float TireTempFrontRight;
+        public float TireTempRearLeft;
+        public float TireTempRearRight;
+        public float Boost;
+        public float Fuel;
+        public float DistanceTraveled;
+        public float BestLap;
+        public float LastLap;
+        public float CurrentLap;
+        public float CurrentRaceTime;
+        public short LapNumber;
+        public byte RacePosition;
+        public byte Accel;
+        public byte Brake;
+        public byte Clutch;
+        public byte HandBrake;
+        public byte Gear;
+        public sbyte Steer;
+        public sbyte NormalizedDrivingLine;
+        public sbyte NormalizedAIBrakeDifference;
+        
+        public float TireWearFrontLeft;
+        public float TireWearFrontRight;
+        public float TireWearRearLeft;
+        public float TireWearRearRight;
+        
+        // ID for track
+        public int TrackOrdinal;
     }
 }
